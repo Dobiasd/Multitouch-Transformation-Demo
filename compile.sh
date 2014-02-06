ef6ca0a54884c928d4c09abd39b15b1bb5d23247
Dobiasd <harry@daiw.de>
2014-02-06 14:52:37 +0100

    new elm version

--- compile.sh
+++ compile.sh
@@ -1,6 +1,7 @@
 rm -r build
 rm -r cache
+mkdir build
 elm -m --src-dir=./src --runtime=elm-runtime.js src/MultitouchTransformationDemo.elm
-cp $HOME/.cabal/share/Elm-0.10.1/elm-runtime.js ./build
+cp $HOME/.cabal/share/Elm-0.11/elm-runtime.js ./build
 mv ./build/src/MultitouchTransformationDemo.html ./build/index.html
 rm -r ./build/src
